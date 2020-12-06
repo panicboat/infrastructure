a026820f70495a16a2ba6f71af9d0acc71790e9d
panicboat <panicboat@gmail.com>
2020-12-06 15:40:17 +0900

    Refactoring CFn

--- products/cloudformation.sh
+++ products/cloudformation.sh
@@ -11,8 +11,8 @@ do
             env=${2}
             shift
         ;;
-        --project|-p)
-            project=${2}
+        --product|-p)
+            product=${2}
             shift
         ;;
         --template|-t)
@@ -27,28 +27,28 @@ do
     shift
 done
 
-if [ -z "$project" ] || [ ! -d "$SCRIPT_DIR/$project" ]; then
+if [ -z "$product" ] || [ ! -d "$SCRIPT_DIR/$product" ]; then
     while true; do
-        read -p 'What project do you deploy to? : ' project
-        if [ -n "$project" ] && [ -d "$SCRIPT_DIR/$project" ]; then
+        read -p 'What product do you deploy to? : ' product
+        if [ -n "$product" ] && [ -d "$SCRIPT_DIR/$product" ]; then
             break
         fi
     done
 fi
 
-if [ -z "$env" ] || [ ! -d "$SCRIPT_DIR/$project/$env" ]; then
+if [ -z "$env" ] || [ ! -d "$SCRIPT_DIR/$product/$env" ]; then
     while true; do
         read -p 'What environment do you deploy to? ( dev / prd ) : ' env
-        if [ -n "$env" ] && [ -d "$SCRIPT_DIR/$project/$env" ]; then
+        if [ -n "$env" ] && [ -d "$SCRIPT_DIR/$product/$env" ]; then
             break
         fi
     done
 fi
 
-if [ -z "$template" ] || [ ! -f "$SCRIPT_DIR/$project/$env/$template.json" ]; then
+if [ -z "$template" ] || [ ! -f "$SCRIPT_DIR/$product/$env/$template.json" ]; then
     while true; do
         read -p 'What template do you deploy to? : ' template
-        if [ -n "$template" ] && [ -f "$SCRIPT_DIR/$project/$env/$template.json" ]; then
+        if [ -n "$template" ] && [ -f "$SCRIPT_DIR/$product/$env/$template.json" ]; then
             break
         fi
     done
@@ -58,19 +58,18 @@ fi
 # Parameters
 # ------------------------------------------------------------------------------------------------------
 networks=`cat $SCRIPT_DIR/../resources/$env/networks.json | jq -r '. | to_entries | map("\(.key)=\(.value|tostring)") | .[]' | tr '\n' ' ' | awk '{print}'`
-params=`cat $SCRIPT_DIR/$project/$env/$template.json | jq -r '. | to_entries | map("\(.key)=\(.value|tostring)") | .[]' | tr '\n' ' ' | awk '{print}'`
-platform=`cat $SCRIPT_DIR/$project/$env/$template.json | jq -r '.PlatformName'`
-product=`cat $SCRIPT_DIR/$project/$env/$template.json | jq -r '.ProductName'`
+params=`cat $SCRIPT_DIR/$product/$env/$template.json | jq -r '. | to_entries | map("\(.key)=\(.value|tostring)") | .[]' | tr '\n' ' ' | awk '{print}'`
+platform=`cat $SCRIPT_DIR/$product/$env/$template.json | jq -r '.PlatformName'`
 
 # ------------------------------------------------------------------------------------------------------
 # Package
 # ------------------------------------------------------------------------------------------------------
 artifact_bucket=`aws cloudformation list-exports | jq -r '.Exports[]' | jq -r 'select(.Name | test("'$platform':ArtifactBucket")) | .Value'`
 aws cloudformation package \
-    --template-file $SCRIPT_DIR/$project/cfn-stack-$template.yml \
+    --template-file $SCRIPT_DIR/$product/cfn-stack-$template.yml \
     --s3-bucket $artifact_bucket \
-    --s3-prefix projects/$project/cloudformation \
-    --output-template-file $SCRIPT_DIR/$project/$env/.cfn-stack-$template.yml
+    --s3-prefix products/$product/cloudformation \
+    --output-template-file $SCRIPT_DIR/$product/$env/.cfn-stack-$template.yml
 if [ $? -ne 0 ]; then
     exit $?
 fi
@@ -79,8 +78,8 @@ fi
 # Deploy
 # ------------------------------------------------------------------------------------------------------
 aws cloudformation deploy \
-    --template-file $SCRIPT_DIR/$project/$env/.cfn-stack-$template.yml \
-    --stack-name $product-$project-$template \
+    --template-file $SCRIPT_DIR/$product/$env/.cfn-stack-$template.yml \
+    --stack-name $platform-$product-$template \
     --parameter-overrides $params $networks \
     --capabilities CAPABILITY_NAMED_IAM
 if [ $? -ne 0 ]; then
